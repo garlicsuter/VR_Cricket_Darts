8c560406e9383337e19c3d156d6c3473102e5612
garlicsuter <markasuter@gmail.com>
2018-05-10 10:03:25 -0400

    Update Dart_Score.cs
    Turns on an "X" when dart hits this twenty collider. Script goes on the area the dart hits.

--- Dart_Score.cs
+++ Dart_Score.cs
@@ -3,7 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dart_Score : MonoBehaviour {
+	
+	private GameObject redDart;
+	private GameObject Scoreboard;
+	private GameObject TwentyRedAX;
+	
+	void Awake()
+	{
+		Scoreboard = GameObject.Find("Scoreboard");
+		TwentyRedAX = GameObject.Find("20RedAX");
+		redDart =  GameObject.FindWithTag("Red");
+	}
 
+	
+	
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +26,18 @@ public class Dart_Score : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Red"))
+		{
+			Destroy (other.gameObject);
+			//Rigidbody temp = other.GetComponent<Rigidbody>;
+			//temp.velocity = Vector3.zero;
+
+			print ("Red hit 20");
+			TwentyRedAX.GetComponent<Renderer>().enabled = true;
+		}
 	}
 }
